ae24483f4f9ecd4382eb74e195e21d56774077d9
SergiyBoiko <codedva00@gmail.com>
2017-03-21 10:10:17 +0200

    create project

---
+++ src/main/java/com/ghub/sergiy/boiko/SpringJpaHibernateApplication.java
@@ -0,0 +1,12 @@
+package com.ghub.sergiy.boiko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaHibernateApplication.class, args);
+	}
+}
